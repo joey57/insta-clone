ec12b2e73c2af6c26f567e08741f941e91da46fd
joey57 <njorogejoey5@gmail.com>
2022-06-03 15:20:29 +0300

    create user login url and template

--- iclone/settings.py
+++ iclone/settings.py
@@ -130,7 +130,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-LOGIN_REDIRECT_URL = ''
+LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = ''
 LOGIN_URL = ''
 
--- insta/views.py
+++ insta/views.py
@@ -15,8 +15,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for  {username}')
-            return redirect('login.html')
+            messages.success(request, f'Youre account has been created you are now able to login {username}')
+            return redirect('login')
     else:
        form = UserRegisterForm()
     return render(request, 'users/register.html',{"form":form})
