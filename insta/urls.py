6d6361b3c5af2cac01adf33199e8c15095fb7106
joey57 <njorogejoey5@gmail.com>
2022-06-03 12:55:48 +0300

    install pillow and set up media folder

--- insta/urls.py
+++ insta/urls.py
@@ -6,3 +6,6 @@ from django.conf.urls.static import static
 urlpatterns=[
   path('', views.index , name = 'index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
