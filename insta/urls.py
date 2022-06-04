830940a132f8c429bc2367ec87b7f6a96570bed7
joey57 <njorogejoey5@gmail.com>
2022-06-04 15:04:37 +0300

    add url link for user profile and search

--- insta/urls.py
+++ insta/urls.py
@@ -6,8 +6,12 @@ from django.conf.urls.static import static
 urlpatterns=[
   path('', views.index , name = 'index'),
   path('upload/',views.upload_picture, name='upload'),
-  path('add_comment/<int:post_id>',views.add_comment, name='add_comment'),
-  path('post/<int:pk>',views.view_post, name='view_post'),
+  path('add_comment/<int:post_id>/',views.add_comment, name='add_comment'),
+  path('post/<int:pk>/',views.view_post, name='view_post'),
+  path('user_profile/<username>/',views.user_profile, name='user_profile'),
+  path('follow/<int:id>/',views.follow, name='follow'),
+  path('unfollow/<int:id>/',views.unfollow, name='unfollow'),
+  path('search/',views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
