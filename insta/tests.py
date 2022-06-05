2bb2a30269830fc27947d8a258a2f45498ef46bb
joey57 <njorogejoey5@gmail.com>
2022-06-05 16:44:39 +0300

    adding test for models

--- insta/tests.py
+++ insta/tests.py
@@ -6,7 +6,7 @@ class ImageTestClass(TestCase):
     def setUp(self):
         self.user = User(username='alex')
         self.user.save()
-        self.user_profile = Profile(user=self.user,profile_picture="mypic.png")
+        self.user_profile = Profile(user=self.user,image="mypic.png")
         self.post = Image(name="views",caption="my views",user=self.user_profile)
         
     def tearDown(self):
@@ -18,18 +18,60 @@ class ImageTestClass(TestCase):
         self.assertTrue(isinstance(self.post,Image))
             
     def test_save_post(self):
-        self.post.save_post()
+        self.post.save_image()
         posts=Image.objects.all()
         self.assertTrue(len(posts)>0)
         
-    def test_update_caption(self):
-        self.post.save_post()
-        self.post.update_post_caption(self.post.id,'test')
-        updated= Image.objects.get(caption='test')
-        self.assertEqual(updated.caption,'test') 
-        
     def test_delete_post(self):
-        self.post.save_post()
-        self.post.delete_post()
+        self.post.save_image()
+        self.post.delete_image()
         posts=Image.objects.all()
-        self.assertTrue(len(posts)==0)              
+        self.assertTrue(len(posts)==0)   
+
+class CommentsTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='alex')
+        self.user.save()
+        self.user_profile = Profile(user=self.user,image="mypic.png")
+        self.post = Image(name="views",caption="my views",user=self.user_profile)
+        self.post.save()
+        self.comment = Comments(comment="Awsome",post=self.post,user=self.user)
+        
+    def tearDown(self):
+        Image.objects.all().delete()
+        User.objects.all().delete()
+        Comments.objects.all().delete()
+        Profile.objects.all().delete()
+                    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Comments))
+
+    def test_delete_comment(self):
+        self.comment.save_comment()
+        self.comment.delete_comment()
+        comments=Comments.objects.all()
+        self.assertTrue(len(comments)==0) 
+
+    def test_save_comment(self):
+        self.comment.save_comment()
+        comments=Comments.objects.all()
+        self.assertTrue(len(comments)>0)
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='alex')
+        
+        self.user_profile = Profile(user=self.user,image="mypic.png")
+   
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+      
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_profile,Profile))
+            
+    def test_save_user_profile(self):
+        self.user_profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+        
