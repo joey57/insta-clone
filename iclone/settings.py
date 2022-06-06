7ef02b0b9480770a684b8b4af027c602ce86cc78
joey57 <njorogejoey5@gmail.com>
2022-06-06 19:38:34 +0300

    deploy to heroku

--- iclone/settings.py
+++ iclone/settings.py
@@ -16,6 +16,12 @@ import cloudinary.uploader
 import cloudinary.api
 import os
 import django_heroku
+import dj_database_url
+from decouple import config, Csv
+
+MODE=config("MODE", default='dev')
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config('DEBUG', default=False, cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +31,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6^kjsci%@3tc-skz6n)9oc==v)c$oj0qywz(r&4=m+mj1&(i5e'
+# SECRET_KEY = 'django-insecure-6^kjsci%@3tc-skz6n)9oc==v)c$oj0qywz(r&4=m+mj1&(i5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'herokuapp.com']
 
 
 # Application definition
@@ -84,14 +90,29 @@ WSGI_APPLICATION = 'iclone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'insta',
-        'USER': 'jkinu',
-        'PASSWORD': 'whalien52'
-    }
-}
+#development
+if config('MODE')=="dev":
+      DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)   
 
 
 # Password validation
